c67eb82f730e918a92fdf8a163fd616bbb43f650
amig05c <amitghosh0595c@gmail.com>
2022-09-28 22:39:07 +0530

    pi and barchart added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Home from './pages/Home';
-
+import Dashboard from './pages/Dashboard';
 function App() {
   return (
     <div className="App">
+      <Dashboard/>
       <Home/>
     </div>
   );
