7f19f514f733f3917e76b15eaaebac70cdeef195
amig05c <amitghosh0595c@gmail.com>
2022-09-29 12:09:57 +0530

    wrap router with index and import files in mainRoutes

--- frontend/src/pages/MainRoutes.jsx
+++ frontend/src/pages/MainRoutes.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
+import Navbar from '../Components/Navbar';
+import Home from "./Home"
+import Login from "./Login"
+import Signup from "./Signup"
+import Footer from "../Components/Footer"
+
 const MainRoutes = () => {
   return (
    <>
    <Routes>
-   <Route path="/"element={<><Navbar /><Homepage /><Footer /></>}></Route>
+   <Route path="/"element={<><Navbar /><Home /><Footer /></>}></Route>
    <Route path="/login" element={<Login />}></Route>
    <Route path="/signup" element={<Signup />}></Route>
    </Routes>
