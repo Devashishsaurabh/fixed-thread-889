70e6d62c831e91a18cb6dd90c308b2f5629c7771
amig05c <amitghosh0595c@gmail.com>
2022-09-29 23:32:05 +0530

    data map in time track page

--- frontend/src/pages/AfterLogin/TimeTracker.jsx
+++ frontend/src/pages/AfterLogin/TimeTracker.jsx
@@ -8,6 +8,7 @@ const TimeTracker = () => {
   const [watch, setWatch]= useState(0)
   const [minute,setminute]=useState(0)
   const [hour,setHour]=useState(0)
+  const [data,setData]= useState([])
   // const [timerId,setTimerId]= useState()
   const timerId=useRef(null);
   
@@ -34,6 +35,8 @@ const TimeTracker = () => {
 
   const reset = ()=>{
       clearInterval(timerId.current)
+      let total= 3600*Number(hour)+60*Number(minute) + Number(watch) 
+      setData([...data,total])
       setWatch(0)
       setminute(0)
       setHour(0)
@@ -55,18 +58,26 @@ if(minute==60){
 
 
   return (
-    <Box w="80vw" h="50vh" border={"1px solid red"}>
-     <Flex w="80%" border={"1px solid red"} m="auto" p="1rem" justifyContent={"space-between"}>
+    <Box w="80vw" h="100vh" border={"1px solid red"} bg="#e4eaee">
+     <Flex w="80%" border={"1px solid red"} m="auto" p="1rem" justifyContent={"space-between"}bg="white">
      <Textarea w="40%" placeholder='Here is a sample placeholder' />
      <Flex w="10%" >
       <AiOutlinePlusCircle fontSize={"30px"}/>
       <Text>Project</Text>
       </Flex>
      <Box w="10%"><BsTags fontSize={"30px"}/></Box>
-     <Box w="10%">{`Time: ${hour}: ${minute}: ${watch} `}</Box>
+     <Box w="10%"><Text as={"b"}>{`Time: ${hour}: ${minute}: ${watch} `}</Text> </Box>
      <Button width={"15%"} onClick={start}>Start</Button>
      <Button width={"15%"} onClick={reset}>Stop</Button>
      </Flex>
+     
+     {data?.map(el=>(
+      <Flex w="70vw" h="5rem" bg={"white"} gap="1rem" m={"1rem"}>
+        <Box>Project</Box>
+        <Box>{el}</Box>
+      </Flex>
+     ))}
+
     </Box>
   )
 }
