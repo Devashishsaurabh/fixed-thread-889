c98db6b4ff3148eed67df00f20798faf417d2ceb
Harshada <harshadajagtap199725@gmail.com>
2022-10-01 16:41:49 +0530

    Hoc

--- frontend/src/pages/MainRoutes.jsx
+++ frontend/src/pages/MainRoutes.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Navbar from "../Components/Navbar"
-import Home from "./Home"
-import Footer from "../Components/Footer"
-import Login from "./Login"
-import Signup from "./Signup"
-import AfterLoginRoutes from './AfterLogin/AfterLoginRoutes';
-
-
+import Navbar from "../Components/Navbar";
+import Home from "./Home";
+import Footer from "../Components/Footer";
+import Login from "./Login";
+import Signup from "./Signup";
+import AfterLoginRoutes from "./AfterLogin/AfterLoginRoutes";
+import RequiredAuth from "../HOC/RequiredAuth";
 
 const MainRoutes = () => {
   return (
-   <>
-   <Routes>
-   <Route path="/"element={<><Navbar /><Home /><Footer /></>}></Route>
-   <Route path="/login" element={<Login />}></Route>
-   <Route path="/signup" element={<Signup />}></Route>
-   <Route path="/clockify/*"element={<AfterLoginRoutes/>}></Route>
-   </Routes>
-   
-   </>
-  )
-}
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Home />
+              <Footer />
+            </>
+          }
+        ></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/clockify/*" element={
 
-export default MainRoutes
+          <RequiredAuth>
+            <AfterLoginRoutes />
+          </RequiredAuth>
+        }></Route>
+      </Routes>
+    </>
+  );
+};
+
+export default MainRoutes;
