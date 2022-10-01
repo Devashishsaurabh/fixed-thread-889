28bb8e207b1882f6042e44a0d943909df165040a
Ayushi0516 <ayushigupta0516@gmail.com>
2022-10-02 00:22:27 +0530

    homepage

--- frontend/src/Components/Home/TimeKeeping.jsx
+++ frontend/src/Components/Home/TimeKeeping.jsx
@@ -3,7 +3,7 @@ import "../styles/TimeKeeping.css"
 const TimeKeeping=()=>{
     return(
         <div className="keep">
-            {/* <h2 className="t">TIMEKEEPING</h2> */}
+           
             <div className="text">
 
               <div className="bold">
@@ -39,22 +39,3 @@ const TimeKeeping=()=>{
 }
 export default TimeKeeping
 
-
-{/* <div className="bold">
-<h4 className="black">Timesheet</h4>
-<p className="lightBlack">Enter time in a weekly timesheet.</p>
-</div>
-<div className="bold">
-<h4 className="black">Calender</h4>
-<p className="lightBlack">Visually block out and manage time.</p>
-</div>
-
-<div className="bold">
-<h4 className="black">Auto tracker</h4>
-<p className="lightBlack">Track apps and websites you see.</p>
-</div>
-
-<div className="bold">
-<h4 className="black">Kiosk</h4>
-<p className="lightBlack">Clock in from a shared device.</p>
-</div> */}
